4ca6820d7f4cb6d07fb3f1d425316062d56b7681
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2024-11-16 21:00:23 -0300

    Implementando exception handler no serviço de pedidos

--- src/core/order/order.module.ts
+++ src/core/order/order.module.ts
@@ -3,13 +3,15 @@ import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
 import { ErrorExceptionFilters } from 'src/shared/utils/services/httpResponseService/errorResponse.service';
 import { PrismaService } from 'src/shared/prisma/prisma.service';
+import { ExceptionHandler } from 'src/shared/utils/services/exceptions/exceptions-handler';
 
 @Module({
   controllers: [OrderController],
   providers: [
     OrderService, 
     ErrorExceptionFilters,
-    PrismaService
+    PrismaService,
+    ExceptionHandler
   ],
 })
 export class OrderModule {}
