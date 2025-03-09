bf4929929c0da88e7ec85756e87acb3fa3feaf6f
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2025-03-09 00:54:33 -0300

    Atualizando campos dos tipos

--- src/shared/types/auditing.d.ts
+++ src/shared/types/auditing.d.ts
@@ -1,14 +1,13 @@
 export interface AuditingModel {
     idUser: number,
-    entity: string,
     changeType: string,
     operation: string,
     description: string
 }
 
 export interface DescriptionAuditingModel {
-    idEntity: number,
     action: string,
+    entity: string,
     previousValue: string,
     newValue: string
 }
