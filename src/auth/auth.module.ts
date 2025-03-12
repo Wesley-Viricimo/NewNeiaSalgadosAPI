e8c206fac83d6afd2074125def22fdfd9b67ff76
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2025-03-11 21:40:52 -0300

    Adicionando serviço de auditoria no módulo de autenticação

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -9,6 +9,7 @@ import { NotificationService } from 'src/service/notification.service';
 import { ErrorExceptionFilters } from 'src/shared/utils/httpResponseService/errorResponse.service';
 import { EmailService } from 'src/service/aws/send-email.service';
 import { ExceptionHandler } from 'src/shared/utils/exceptions/exceptions-handler';
+import { AuditingService } from 'src/service/auditing.service';
 
 @Module({
   imports: [HttpModule],
@@ -21,7 +22,8 @@ import { ExceptionHandler } from 'src/shared/utils/exceptions/exceptions-handler
     TokenService,
     EmailService,
     ExceptionHandler,
-    NotificationService
+    NotificationService,
+    AuditingService
   ],
 })
 export class AuthModule {}
