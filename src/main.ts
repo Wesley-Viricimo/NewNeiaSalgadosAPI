d09e68bdc83969f3c489a8bd1708b83ca7e229cf
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2025-04-14 21:36:11 -0300

    Add @fastify/cors dependency and configure CORS in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
-import { contentParser } from "fastify-file-interceptor";
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import fastifyCors from '@fastify/cors';
+import { contentParser } from 'fastify-file-interceptor';
 
 async function bootstrap() {
+  const fastifyAdapter = new FastifyAdapter();
+
+  // 2) Registra o plugin de CORS ANTES de criar a app
+  fastifyAdapter.register(fastifyCors, {
+    origin: ['http://localhost:1420'], // sua URL do frontend
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  });
+
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter()
+    fastifyAdapter,
   );
+
   app.useGlobalPipes(new ValidationPipe());
   await app.register(contentParser);
-  app.enableCors();
+
   await app.listen(3000);
 }
 bootstrap();
