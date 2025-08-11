efa490f4b323177132b538e97dbd0d03c5306860
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2025-08-10 23:35:24 -0300

    feat: update Notification service and controller to handle marking notifications as read and return structured responses

--- src/core/notification/notification.controller.ts
+++ src/core/notification/notification.controller.ts
@@ -1,7 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus, Param, Post, Req } from '@nestjs/common';
 import { NotificationService } from './notification.service';
+import { FastifyRequest } from 'fastify';
+import { Roles } from 'src/shared/decorators/rolesPermission.decorator';
 
-@Controller('notification')
+@Controller('api/v1/notification')
 export class NotificationController {
-  constructor(private readonly notificationService: NotificationService) {}
+  constructor(private readonly notificationService: NotificationService) { }
+
+  @Roles('ADMIN', 'DEV', 'COMERCIAL')
+  @Get()
+  getAllUnreadNotifications() {
+    return this.notificationService.getAllUnreadNotifications();
+  }
+
+  @Roles('ADMIN', 'DEV', 'COMERCIAL')
+  @Post('markAsRead/:idNotification')
+  @HttpCode(HttpStatus.CREATED)
+  markNotificationAsRead(
+    @Param('idNotification') idNotification: string,
+    @Req() request: FastifyRequest
+  ) {
+    return this.notificationService.markNotificationAsRead(+idNotification, request['userId']);
+  }
+
 }
--- src/core/notification/notification.service.ts
+++ src/core/notification/notification.service.ts
@@ -1,4 +1,62 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/shared/prisma/prisma.service';
+import { ExceptionHandler } from 'src/shared/utils/exceptions/exceptions-handler';
 
 @Injectable()
-export class NotificationService {}
+export class NotificationService {
+
+    constructor(
+        private readonly prismaService: PrismaService,
+        private readonly exceptionHandler: ExceptionHandler
+    ) { }
+
+    async getAllUnreadNotifications() {
+        return await this.prismaService.notification.findMany({
+            where: { read: false },
+            select: {
+                idNotification: true,
+                description: true,
+                read: true
+            }
+        })
+            .then(response => {
+                const message = { severity: 'success', summary: 'Sucesso', detail: 'Notificações listadas com sucesso.' };
+                return {
+                    data: response,
+                    message,
+                    statusCode: HttpStatus.OK
+                }
+            })
+    }
+
+    async markNotificationAsRead(idNotification: number, idUser: number) {
+        try {
+            const [notification, user] = await Promise.all([
+                this.prismaService.notification.findUnique({ where: { idNotification } }),
+                this.prismaService.user.findUnique({ where: { idUser } }),
+            ]);
+
+            if (!notification)
+                this.exceptionHandler.errorBadRequestResponse('Notificação não foi encontrada!');
+            if (!user)
+                this.exceptionHandler.errorBadRequestResponse('Usuário não cadastrado no sistema!');
+
+            const response = await this.prismaService.notificationRead.create({
+                data: { idNotification, idUser }
+            });
+
+            await this.prismaService.notification.update({
+                where: { idNotification },
+                data: { read: true }
+            });
+
+            return {
+                data: response,
+                message: { severity: 'success', summary: 'Sucesso', detail: 'Notificação lida com sucesso.' },
+                statusCode: HttpStatus.CREATED
+            };
+        } catch {
+            this.exceptionHandler.errorBadRequestResponse('Erro ao marcar notificação como lida!');
+        }
+    }
+}
